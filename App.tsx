a6eb7a1ffe1b9b9fcd62997334b2527229f72d20
Jakub <kuba.wasik.5a@gmail.com>
2020-03-19 08:43:01 +0100

    Test Cheerio

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import cheerio from 'react-native-cheerio';
 
 export default function App() {
+
+  useEffect( ()=> {
+    const test_parse = async () => {
+      const googleSite = await fetch("https://www.google.com");
+
+      const $ = cheerio.load(await googleSite.text());
+
+      const footer = $("#footer").text();
+
+      console.log(footer);
+    }
+
+    test_parse();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
