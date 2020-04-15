9dae5d98aed3766075cb942b47649e119f2fbff9
azalesny <adrian.zalesny@gmail.com>
2020-04-15 22:31:47 +0200

    WU-6 WU-17 login function and local Stor

--- App.tsx
+++ App.tsx
@@ -5,30 +5,12 @@ import SplashScreen from "./screens/SplashScreen";
 import { axiosInstance, LOGIN_URL } from "./session/Session";
 
 export default function App() {
-  const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [login, setLogin] = React.useState(false);
+  
 
   useEffect(() => {
     const test_login = async () => {
-      setLoading(true);
-      const login = "";
-      const password = "";
-      const loginData =
-        "ctl00_ctl00_ScriptManager1_HiddenField=&" +
-        "__EVENTTARGET=&" +
-        "__EVENTARGUMENT=&" +
-        "__VIEWSTATE=&" +
-        "__VIEWSTATEGENERATOR=&" +
-        "ctl00_ctl00_TopMenuPlaceHolder_TopMenuContentPlaceHolder_MenuTop3_menuTop3_ClientState=&" +
-        "ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24txtIdent=" +
-        login +
-        "&" +
-        "ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24txtHaslo=" +
-        password +
-        "&" +
-        "ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24butLoguj=Zaloguj";
-
-      const response = await axiosInstance.post(LOGIN_URL, { data: loginData });
-      //Redirect KierunkiStudiow
       setLoading(false);
     };
     test_login();
@@ -36,7 +18,7 @@ export default function App() {
 
   return (
     <PaperProvider>
-      {loading ? <SplashScreen /> : <LoginScreen />}
+      {loading ? <SplashScreen /> : (login ? <SplashScreen/>: <LoginScreen setLogin={setLogin}/> ) }
     </PaperProvider>
   );
 }
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,22 +1,50 @@
+import * as SecureStore from "expo-secure-store";
 import * as React from "react";
-import { Dimensions, Image, KeyboardAvoidingView, Platform, Alert } from "react-native";
+import {Alert, AsyncStorage, Dimensions, Image, KeyboardAvoidingView, Platform} from "react-native";
 import { Button, Headline, HelperText, TextInput } from "react-native-paper";
 import { ScaledSheet } from "react-native-size-matters";
-import { axiosInstance } from "../session/Session";
+import {axiosInstance, LOGIN_URL} from "../session/Session";
+import cheerio from "react-native-cheerio";
+import {AxiosRequestConfig} from "axios";
+import qs from "qs";
+
 
 const logo = require("../assets/logoUP.png");
 const keyboardVerticalOffset = Platform.OS === "ios" ? 40 : 0;
 const window = Dimensions.get("window");
 const IMAGE_SIZE = window.width / 2;
 
-function LoginScreen() {
+const LoginScreen = (props) => {
   const [login, setLogin] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [isLoginEmpty, setIsLoginEmpty] = React.useState(false);
   const [isPasswordEmpty, setIsPasswordEmpty] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        // Retrieve the credentials
+        const loginUser = await SecureStore.getItemAsync("loginUser");
+        const passwordUser = await SecureStore.getItemAsync("passwordUser");
 
-  const inputLogin = React.useRef(null);
-  const inputPassword = React.useRef(null);
+        // if null or empty or undefined this will be eval to false
+        if (loginUser && passwordUser) {
+          setLogin(loginUser);
+          setPassword(passwordUser);
+          console.log("Credentials successfully loaded for user " + loginUser);
+        } else {
+          console.log("No credentials stored");
+        }
+      } catch (error) {
+        console.log("SecureStore couldn't be accessed!", error);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
 
   const handleSubmit = async () => {
     if (login.length === 0) {
@@ -35,19 +63,49 @@ function LoginScreen() {
       return;
     }
 
-    // TODO: login function
+    setLoading(true);
 
-    // const response = await axiosInstance.post( some URL, some data, some config );
+    const loginGetResponse = await axiosInstance.get(LOGIN_URL);
+    const login$ = cheerio.load(loginGetResponse.data);
+    const loginInputsToPost = {};
+    const hiddenLoginInputs = login$("input[type='hidden']");
+    hiddenLoginInputs.each((index, element) => {
+      loginInputsToPost[element.attribs.name] = element.attribs.value
+        ? element.attribs.value
+        : "";
+    });
 
-    // alert error if fail, well now it will always fail
+    loginInputsToPost[
+      "ctl00$ctl00$ContentPlaceHolder$MiddleContentPlaceHolder$txtIdent"
+      ] = login;
+    loginInputsToPost[
+      "ctl00$ctl00$ContentPlaceHolder$MiddleContentPlaceHolder$txtHaslo"
+      ] = password;
+    loginInputsToPost[
+      "ctl00$ctl00$ContentPlaceHolder$MiddleContentPlaceHolder$butLoguj"
+      ] = "Zaloguj";
+
+    const loginOptions: AxiosRequestConfig = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: qs.stringify(loginInputsToPost),
+      url: LOGIN_URL,
+    };
+
+    const response1 = await axiosInstance(loginOptions);
+    if(response1.request.responseURL.includes("KierunkiStudiow")){
+      await SecureStore.setItemAsync("loginUser", login);
+      await SecureStore.setItemAsync("passwordUser", password);
+      props.setLogin(true);
+    }
     
-    Alert.alert(
-      "Błąd",
-      "Nie udało się zalogować, wystąpił błąd, proszę spróbować ponownie",
-      [{ text: "OK" }],
-      { cancelable: false }
-    );
+    setLoading(false);
   };
+
+  const inputLogin = React.useRef(null);
+  const inputPassword = React.useRef(null);
 
   return (
     <KeyboardAvoidingView
@@ -63,7 +121,7 @@ function LoginScreen() {
         style={styles.textInput}
         mode="outlined"
         ref={inputLogin}
-        onChangeText={value => setLogin(value)}
+        onChangeText={(value) => setLogin(value)}
         onSubmitEditing={() => inputPassword.current.focus()}
         returnKeyType={"next"}
       />
@@ -76,7 +134,7 @@ function LoginScreen() {
         style={styles.textInput}
         mode="outlined"
         ref={inputPassword}
-        onChangeText={value => setPassword(value)}
+        onChangeText={(value) => setPassword(value)}
         secureTextEntry={true}
       />
       <HelperText
@@ -97,22 +155,22 @@ function LoginScreen() {
       </Button>
     </KeyboardAvoidingView>
   );
-}
+};
 
 const styles = ScaledSheet.create({
   container: {
     flex: 1,
-    margin: 25
+    margin: 25,
   },
   header: {
     height: "40@s",
     textAlign: "center",
     fontSize: "20@s",
-    paddingTop: 10
+    paddingTop: 10,
   },
   textInput: {
     height: "50@s",
-    fontSize: "12@s"
+    fontSize: "12@s",
   },
   loginButton: {
     height: "50@s",
@@ -120,23 +178,55 @@ const styles = ScaledSheet.create({
     alignSelf: "center",
     width: "50%",
     justifyContent: "center",
-    maxHeight: 50
+    maxHeight: 50,
   },
   loginButtonText: {
-    fontSize: "10@s"
+    fontSize: "10@s",
   },
   logo: {
     flexBasis: "40%",
     resizeMode: "contain",
     height: IMAGE_SIZE,
     width: IMAGE_SIZE,
-    alignSelf: "center"
+    alignSelf: "center",
   },
   helperText: {
     height: "30@s",
     alignSelf: "center",
-    fontSize: "10@s"
-  }
+    fontSize: "10@s",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: "20@s",
+    marginBottom: "60@s",
+    borderRadius: 4,
+    borderColor: "rgba(0, 0, 0, 0.1)",
+  },
+  modalHeader: {
+    textAlign: "center",
+    paddingTop: "10@s",
+    paddingBottom: "10@s",
+    fontSize: "22@s",
+    lineHeight: "20@s",
+  },
+  modalText: {
+    fontSize: "14@s",
+    textAlign: "center",
+  },
+  button: {
+    width: "50%",
+    marginTop: "20@s",
+    marginBottom: "20@s",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  buttonText: {
+    fontSize: "12@s",
+  },
+  divider: {
+    margin: "20@s",
+    padding: 1,
+  },
 });
 
 export default LoginScreen;
