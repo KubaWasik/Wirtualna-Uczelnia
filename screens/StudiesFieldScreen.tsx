0deb6188f3f901e3b77a7ec7022caef3cf9735c8
osiou <ncpnc69>
2020-04-30 02:57:55 +0200

    StudiesField UI ImPrOvEMeNt

--- screens/StudiesFieldScreen.tsx
+++ screens/StudiesFieldScreen.tsx
@@ -1,17 +1,31 @@
+import { StackNavigationProp } from "@react-navigation/stack";
+import { Updates } from "expo";
 import * as React from "react";
 import { Alert, AsyncStorage, StyleSheet, Text, View } from "react-native";
 import { Appbar, Button, Checkbox, RadioButton, TouchableRipple } from "react-native-paper";
+import { RootStackParamList } from "../App";
+import * as Errors from "../errors/Errors";
+import fetchAllData from "../session/FetchAllData";
 import { fetchStudiesField, IField, setStudiesField } from "../session/FetchStudiesField";
 import LoadingOverview from "./LoadingOverview";
 
-function StudiesFieldScreen({ navigation }) {
+type StudiesFieldScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "StudiesField"
+>;
+
+type Props = {
+  navigation: StudiesFieldScreenNavigationProp;
+};
+
+function StudiesFieldScreen(props: Props) {
   const [loading, setLoading] = React.useState(false);
   const [value, setValue] = React.useState(0);
   const [checked, setChecked] = React.useState(false);
   const [fields, setFields] = React.useState<IField[]>([]);
 
-  const chooseStudiesField = async (field: number) => {
-    if (field == 0) {
+  const chooseStudiesField = async (field: string) => {
+    if (field === "0") {
       Alert.alert(
         "Uwaga",
         "Proszę wybrać jeden z kierunków",
@@ -23,15 +37,43 @@ function StudiesFieldScreen({ navigation }) {
     }
 
     setLoading(true);
-    const response = await setStudiesField(field, checked);
-    setLoading(false);
 
-    if (response === "SUCCESS") {
-      navigation.navigate("Home");
-    } else {
-      Alert.alert("Błąd", "Podczas wyboru kierunku wystąpił błąd. " + response);
+    try {
+      await setStudiesField(field, checked);
+      await fetchAllData();
+
+      setLoading(false);
+      props.navigation.navigate("Home");
+    } catch (error) {
+      if (error instanceof Errors.AxiosNetwork) {
+        // Timeout - suggest offline mode
+        console.log("Timeout - suggest offline mode");
+        Alert.alert(
+          "Błąd",
+          "Nastąpił błąd połączenia, Twoja sieć może mieć za słaby zasięg, spróbuj ponownie. Jeśli widzisz to kolejny raz możesz spróbować wejśc do trybu offline zamiast tego",
+          [
+            { text: "Zamknij", onPress: () => null },
+            { text: "Spróbuj ponownie", onPress: () => Updates.reload() },
+            {
+              text: "Tryb offline",
+              onPress: () => null /* TODO: make it works*/,
+            },
+          ],
+          { cancelable: false }
+        );
+      } else {
+        // Other errors - suggest restart
+        Alert.alert(
+          "Błąd",
+          "Nastąpił nieoczekiwany błąd, spróbuj ponownie.",
+          [
+            { text: "Zamknij", onPress: () => null },
+            { text: "Spróbuj ponownie", onPress: () => Updates.reload() },
+          ],
+          { cancelable: false }
+        );
+      }
     }
-
   };
 
   React.useEffect(() => {
@@ -101,7 +143,7 @@ function StudiesFieldScreen({ navigation }) {
         </TouchableRipple>
         <Button
           mode="contained"
-          onPress={() => chooseStudiesField(value)}
+          onPress={() => chooseStudiesField(value.toString())}
           style={styles.submitButton}
         >
           Wybierz
