b9b4d762c43d7b8c1c52939da4e8e9702b26bcfa
Jakub <kuba.wasik.5a@gmail.com>
2020-05-14 00:03:08 +0200

    WU-25 Grades UI improvement + WU-26

--- session/FetchGrades.tsx
+++ session/FetchGrades.tsx
@@ -1,22 +1,21 @@
-import { GRADES_URL, getAxiosInstance } from "./Session";
+import { AxiosInstance } from "axios";
 import cheerio from "react-native-cheerio";
+import { GRADES_URL } from "./Session";
 
-// TODO: na pewno nie wysztko będzie stringiem, zmienić tak by miało to sens
 export interface IGrade {
-  index: number;
-  courseName: string;
-  type: string;
   graduator: string;
   numberOfHours: number;
   firstTerm: string;
-  corrective: string;
+  secondTerm: string;
   comission: string;
   ects: number;
 }
 
-async function fetchGrades(): Promise<IGrade[]> {
-  const axiosInstance = await getAxiosInstance();
+export interface IGrades {
+  [courseName: string]: { [gradeType: string]: IGrade };
+}
 
+async function fetchGrades(axiosInstance: AxiosInstance): Promise<IGrades> {
   const response = await axiosInstance.get(GRADES_URL);
 
   const $ = cheerio.load(response.data);
@@ -25,31 +24,36 @@ async function fetchGrades(): Promise<IGrade[]> {
     "#ctl00_ctl00_ContentPlaceHolder_RightContentPlaceHolder_dgDane > tbody > tr"
   );
 
-  var grades: IGrade[] = [];
+  var grades: IGrades = {};
 
   for (let index = 1; index < table.length; index++) {
     const courseName = table[index].children[0].children[0].data;
-    const type = table[index].children[1].children[0].data;
+    const gradeType = table[index].children[1].children[0].data;
     const graduator = table[index].children[2].children[0].data;
     const numberOfHours = table[index].children[3].children[0].data;
     const firstTerm = table[index].children[4].children[0].data;
-    const corrective = table[index].children[5].children[0].data;
+    const secondTerm = table[index].children[5].children[0].data;
     const comission = table[index].children[6].children[0].data;
-    const ECTS = table[index].children[7].children[0].data;
+    const ects = table[index].children[7].children[0].data;
 
-    var item: IGrade = {
-      index: index,
-      courseName: courseName,
-      type: type,
+    var gradeItem: IGrade = {
+      comission: comission,
+      ects: Number.parseInt(ects),
+      firstTerm: firstTerm,
       graduator: graduator,
       numberOfHours: Number.parseInt(numberOfHours),
-      firstTerm: firstTerm,
-      corrective: corrective,
-      comission: comission,
-      ects: Number.parseInt(ECTS),
+      secondTerm: secondTerm,
     };
 
-    grades.push(item);
+    if (courseName in grades) {
+      var tmp = grades[courseName];
+      tmp[gradeType] = gradeItem;
+      grades[courseName] = tmp;
+    } else {
+      var tmp: { [gradeType: string]: IGrade } = {};
+      tmp[gradeType] = gradeItem;
+      grades[courseName] = tmp;
+    }
   }
 
   return grades;
